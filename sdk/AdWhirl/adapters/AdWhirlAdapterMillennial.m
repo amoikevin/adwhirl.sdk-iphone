58c8ff03bf61c210ef4c6de62887c3d60c99173b
Nigel Choi <nigel@admob.com>
2009-12-03 17:20:01 -0800

    First commit.

---
+++ sdk/AdWhirl/adapters/AdWhirlAdapterMillennial.m
@@ -0,0 +1,166 @@
+/*
+
+ AdWhirlAdapterMillennial.m
+ 
+ Copyright 2009 AdMob, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ 
+*/
+
+#import "AdWhirlAdapterMillennial.h"
+#import "AdWhirlView.h"
+#import "AdWhirlConfig.h"
+#import "AdWhirlAdNetworkConfig.h"
+#import "AdWhirlDelegateProtocol.h"
+#import "AdWhirlLog.h"
+#import "AdWhirlAdNetworkAdapter+Helpers.h"
+#import "AdWhirlAdNetworkRegistry.h"
+
+@implementation AdWhirlAdapterMillennial
+
++ (AdWhirlAdNetworkType)networkType {
+  return AdWhirlAdNetworkTypeMillennial;
+}
+
++ (void)load {
+  [[AdWhirlAdNetworkRegistry sharedRegistry] registerClass:self];
+}
+
+- (void)getAd {
+  MMAdView *adView = [[MMAdView alloc] initWithFrame:kAdWhirlViewDefaultFrame];
+  if ([adWhirlDelegate respondsToSelector:@selector(millennialMediaApIDString)]) {
+    adView.apID = [adWhirlDelegate millennialMediaApIDString];
+  }
+  else {
+    adView.apID = networkConfig.pubId;
+  }
+  adView.delegate = self;
+  self.adNetworkView = adView;
+  [adView release];
+}
+
+- (void)dealloc {
+  [super dealloc];
+}
+
+#pragma mark MMAdViewDelegate methods
+
+- (void)adRequestSucceeded {
+  // millennial ads are slightly taller than default frame, at 53 pixels.
+  [self helperFitAdNetworkView];
+  [adWhirlView adapter:self didReceiveAdView:adNetworkView];
+}
+
+- (void)adRequestFailed {
+  [adWhirlView adapter:self didFailAd:nil];
+}
+
+- (void)adViewWillTakeOver:(MMAdView *)adView {
+  [self helperNotifyDelegateOfFullScreenModal];
+}
+
+- (void)adViewWillReleaseControl:(MMAdView *)adView {
+  [self helperNotifyDelegateOfFullScreenModalDismissal];
+}
+
+#pragma mark MMAdViewDelegate optional methods
+
+- (BOOL)respondsToSelector:(SEL)selector {
+  if (selector == @selector(latitude)
+      && ![adWhirlDelegate respondsToSelector:@selector(locationInfo)]) {
+    return NO;
+  }
+  else if (selector == @selector(longitude)
+           && ![adWhirlDelegate respondsToSelector:@selector(locationInfo)]) {
+    return NO;
+  }
+  else if (selector == @selector(age)
+           && (!([adWhirlDelegate respondsToSelector:@selector(millennialMediaAge)]
+                 || [adWhirlDelegate respondsToSelector:@selector(dateOfBirth)])
+               || [self age] < 0)) {
+    return NO;
+  }
+  else if (selector == @selector(zipCode)
+           && ![adWhirlDelegate respondsToSelector:@selector(postalCode)]) {
+    return NO;
+  }
+  else if (selector == @selector(sex)
+           && ![adWhirlDelegate respondsToSelector:@selector(gender)]) {
+    return NO;
+  }
+  else if (selector == @selector(householdIncome)
+           && ![adWhirlDelegate respondsToSelector:@selector(incomeLevel)]) {
+    return NO;
+  }
+  else if (selector == @selector(educationLevel)
+           && ![adWhirlDelegate respondsToSelector:@selector(millennialMediaEducationLevel)]) {
+    return NO;
+  }
+  else if (selector == @selector(ethnicity)
+           && ![adWhirlDelegate respondsToSelector:@selector(millennialMediaEthnicity)]) {
+    return NO;
+  }
+  return [super respondsToSelector:selector];
+}
+
+- (float)latitude {
+  CLLocation *loc = [adWhirlDelegate locationInfo];
+  if (loc == nil) return 0.0;
+  return loc.coordinate.latitude;
+}
+
+- (float)longitude {
+  CLLocation *loc = [adWhirlDelegate locationInfo];
+  if (loc == nil) return 0.0;
+  return loc.coordinate.longitude;
+}
+
+- (NSInteger)age {
+  if ([adWhirlDelegate respondsToSelector:@selector(millennialMediaAge)]) {
+    return [adWhirlDelegate millennialMediaAge];
+  }
+  return [self helperCalculateAge];
+}
+
+- (NSString*)zipCode {
+  return [adWhirlDelegate postalCode];
+}
+
+- (MMSex)sex {
+  NSString *gender = [adWhirlDelegate gender];
+  MMSex sex = MMSexUnknown;
+  if (gender == nil)
+    return sex;
+  if ([gender compare:@"m"] == NSOrderedSame) {
+    sex = MMSexMale;
+  }
+  else if ([gender compare:@"f"] == NSOrderedSame) {
+    sex = MMSexFemale;
+  }
+  return sex;
+}
+
+- (NSInteger)householdIncome {
+  return (NSInteger)[adWhirlDelegate incomeLevel];
+}
+
+- (MMEducation)educationLevel {
+  return [adWhirlDelegate millennialMediaEducationLevel];
+}
+
+- (MMEthnicity)ethnicity {
+  return [adWhirlDelegate millennialMediaEthnicity];
+}
+
+@end
---
+++ sdk/AdWhirlSDK2_Sample/Classes/RootViewController.m
@@ -0,0 +1,196 @@
+/*
+
+ RootViewController.m
+ 
+ Copyright 2009 AdMob, Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ 
+*/
+
+#import "RootViewController.h"
+#import "SimpleViewController.h"
+#import "TableController.h"
+#import "AdWhirlView.h"
+#import "SampleConstants.h"
+
+
+@implementation RootViewController
+
+/*
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+  // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+*/
+
+/*
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+}
+*/
+/*
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+}
+*/
+/*
+- (void)viewWillDisappear:(BOOL)animated {
+	[super viewWillDisappear:animated];
+}
+*/
+/*
+- (void)viewDidDisappear:(BOOL)animated {
+	[super viewDidDisappear:animated];
+}
+*/
+
+/*
+ // Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	// Return YES for supported orientations.
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+  [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release anything that can be recreated in viewDidLoad or on demand.
+	// e.g. self.myOutlet = nil;
+}
+
+
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 3;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+  static NSString *CellIdentifier = @"Cell";
+    
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+#ifdef __IPHONE_3_0
+    cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+#else
+    cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
+#endif
+  }
+
+  switch (indexPath.row) {
+    case 0:
+#ifdef __IPHONE_3_0
+      cell.textLabel.text = @"Simple View";
+#else
+      cell.text = @"Simple View";
+#endif
+      break;
+    case 1:
+#ifdef __IPHONE_3_0
+      cell.textLabel.text = @"Table Integration";
+#else
+      cell.text = @"Table Integration";
+#endif
+      break;
+    case 2:
+    {
+      NSString *configText;
+      if (configFetched) {
+        configText = @"Config Fetched";
+      }
+      else {
+        configText = @"Prefetch Config";
+      }
+#ifdef __IPHONE_3_0
+      cell.textLabel.text = configText;
+#else
+      cell.text = configText;
+#endif
+      break;
+    }
+  }
+  
+  return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  switch (indexPath.row) {
+    case 0:
+    {
+      SimpleViewController *simple = [[SimpleViewController alloc] initWithNibName:@"SimpleViewController"
+                                                                            bundle:nil];
+      [self.navigationController pushViewController:simple animated:YES];
+      [simple release];
+      break;
+    }
+    case 1:
+    {
+      TableController *table = [[TableController alloc] initWithNibName:@"TableController"
+                                                                 bundle:nil];
+      [self.navigationController pushViewController:table animated:YES];
+      [table release];
+      break;
+    }
+    case 2:
+      if (configFetched) {
+        [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+      }
+      else {
+        [AdWhirlView startPreFetchingConfigurationDataWithDelegate:self];
+      }
+      break;
+  }
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+#pragma mark AdWhirlDelegate methods
+
+- (NSString *)adWhirlApplicationKey {
+  return kSampleAppKey;
+}
+
+- (NSURL *)adWhirlConfigURL {
+  return [NSURL URLWithString:kSampleConfigURL];
+}
+
+- (void)adWhirlDidReceiveConfig:(AdWhirlView *)adWhirlView {
+  NSIndexPath *indexPath = [NSIndexPath indexPathForRow:2 inSection:0];
+  [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+  configFetched = YES;
+  [self.tableView reloadData];
+}
+
+@end
+
