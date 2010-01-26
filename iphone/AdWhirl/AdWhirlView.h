65009be34b230beed408355c4ea4aa961d9c5249
Nigel Choi <nigel@admob.com>
2010-01-26 11:07:24 -0800

    Bump version to 210

--- iphone/AdWhirl/AdWhirlView.h
+++ iphone/AdWhirl/AdWhirlView.h
@@ -22,7 +22,7 @@
 #import "AdWhirlDelegateProtocol.h"
 #import "AdWhirlConfig.h"
 
-#define kAdWhirlAppVer 202
+#define kAdWhirlAppVer 210
 
 #define kAdWhirlViewWidth 320
 #define kAdWhirlViewHeight 50
