d046d615d4d2ea545ffc78917b945a4317ce54be
Nigel Choi <nigel@admob.com>
2010-02-02 10:12:13 -0800

    No need to explicitly release the ad view after animation.

--- iphone/AdWhirl/internal/AdWhirlView.m
+++ iphone/AdWhirl/internal/AdWhirlView.m
@@ -417,7 +417,6 @@ static BOOL randSeeded = NO;
 - (void)newAdAnimationDidStopWithAnimationID:(NSString *)animationID finished:(BOOL)finished context:(void *)context {
   UIView *adViewToRemove = (UIView *)context;
   [adViewToRemove removeFromSuperview];
-  [adViewToRemove release];
 }
 
 - (void)adapter:(AdWhirlAdNetworkAdapter *)adapter didReceiveAdView:(UIView *)view {
