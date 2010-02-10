582128bc7e86d5cf258e3bf44349a480a859a02d
Nigel Choi <nigel@admob.com>
2010-02-09 16:31:56 -0800

    Added a location info test view.

---
+++ iphone/AdWhirlSDK2_Sample/Classes/LocationController.m
@@ -0,0 +1,59 @@
+//
+//  LocationController.m
+//  AdWhirlSDK2_Sample
+//
+//  Created by Nigel Choi on 2/8/10.
+//  Copyright 2010 Admob. Inc.. All rights reserved.
+//
+
+#import "LocationController.h"
+#import "AdWhirlLog.h"
+
+@implementation LocationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
+  }
+  return self;
+}
+
+- (UILabel *)locLabel {
+  return (UILabel *)[self.view viewWithTag:103];
+}
+
+- (void)dealloc {
+  [locationManager release], locationManager = nil;
+  [super dealloc];
+}
+
+
+#pragma mark AdWhirlDelegate methods
+
+- (CLLocation *)locationInfo {
+  CLLocation *loc = [locationManager location];
+  AWLogDebug(@"AdWhirl asking for location: %@", loc);
+  return loc;
+}
+
+
+#pragma mark CLLocationManagerDelegate methods
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error {
+  self.locLabel.text = [NSString stringWithFormat:@"Error getting location: %@",
+                        [error localizedDescription]];
+  AWLogError(@"Failed getting location: %@", error);
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation {
+  self.locLabel.text = [NSString stringWithFormat:@"%lf %lf",
+                        newLocation.coordinate.longitude,
+                        newLocation.coordinate.latitude];
+}
+
+@end
