0d8e283d330392b0740f307948ffa402fb09976e
Nigel Choi <nigel@admob.com>
2010-02-25 15:13:08 -0800

    Enabled landscape mode for table and location example.

--- iphone/AdWhirlSDK2_Sample/Classes/LocationController.m
+++ iphone/AdWhirlSDK2_Sample/Classes/LocationController.m
@@ -9,6 +9,10 @@
 #import "LocationController.h"
 #import "AdWhirlLog.h"
 
+#define LOCVIEW_LOCLABEL_OFFSET 79
+#define LOCVIEW_LABEL_OFFSET 87
+#define LOCVIEW_LABEL_HDIFF 63
+
 @implementation LocationController
 
 - (id)init {
@@ -16,13 +20,52 @@
     locationManager = [[CLLocationManager alloc] init];
     locationManager.delegate = self;
     [locationManager startUpdatingLocation];
+    currLayoutOrientation = UIInterfaceOrientationPortrait; // nib file defines a portrait view
   }
   return self;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  [self adjustLayoutToOrientation:self.interfaceOrientation];
 }
 
 - (UILabel *)locLabel {
   return (UILabel *)[self.view viewWithTag:103];
 }
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+                                         duration:(NSTimeInterval)duration {
+  [self adjustLayoutToOrientation:interfaceOrientation];
+}
+
+- (void)adjustLayoutToOrientation:(UIInterfaceOrientation)newOrientation {
+  UILabel *ll = self.locLabel;
+  UILabel *label = self.label;
+  assert(ll != nil);
+  assert(label != nil);
+  if (UIInterfaceOrientationIsPortrait(currLayoutOrientation)
+      && UIInterfaceOrientationIsLandscape(newOrientation)) {
+    CGPoint newCenter = ll.center;
+    newCenter.y -= LOCVIEW_LOCLABEL_OFFSET;
+    ll.center = newCenter;
+    CGRect newFrame = label.frame;
+    newFrame.origin.y -= LOCVIEW_LABEL_OFFSET;
+    newFrame.size.height -= LOCVIEW_LABEL_HDIFF;
+    label.frame = newFrame;
+  }
+  else if (UIInterfaceOrientationIsLandscape(currLayoutOrientation)
+           && UIInterfaceOrientationIsPortrait(newOrientation)) {
+    CGPoint newCenter = ll.center;
+    newCenter.y += LOCVIEW_LOCLABEL_OFFSET;
+    ll.center = newCenter;
+    CGRect newFrame = label.frame;
+    newFrame.origin.y += LOCVIEW_LABEL_OFFSET;
+    newFrame.size.height += LOCVIEW_LABEL_HDIFF;
+    label.frame = newFrame;
+  }
+  currLayoutOrientation = newOrientation;
+}  
 
 - (void)dealloc {
   locationManager.delegate = nil;
