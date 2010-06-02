1b029bb8c604631c0ae3a141119ea974131eccee
Nigel Choi <nigel@admob.com>
2010-06-02 00:38:45 -0700

    Implemented Google AdSense adapter.

--- iphone/AdWhirl/adapters/AdWhirlAdapterGoogleAdSense.m
+++ iphone/AdWhirl/adapters/AdWhirlAdapterGoogleAdSense.m
@@ -18,26 +18,153 @@
  
 */
 
+#import "AdWhirlAdNetworkAdapter+Helpers.h"
 #import "AdWhirlAdapterGoogleAdSense.h"
+#import "AdWhirlAdNetworkConfig.h"
+#import "AdWhirlConfig.h"
+#import "AdWhirlLog.h"
+
+static NSDictionary *GASParamNameToSel;
 
 @implementation AdWhirlAdapterGoogleAdSense
 
+@synthesize adViewController;
+
 + (NSInteger)networkType {
-  return 14;
+  return AdWhirlAdNetworkTypeGoogleAdSense;
 }
 
-//+ (void)load {
-//  [[AdWhirlAdNetworkRegistry sharedRegistry] registerClass:self];
-//}
++ (void)load {
+	[[AdWhirlAdNetworkRegistry sharedRegistry] registerClass:self];
+  GASParamNameToSel = [[NSDictionary alloc] initWithObjectsAndKeys:
+                     @"googleAdSenseKeywords", kGADAdSenseKeywords,
+                     @"googleAdSenseAppWebContentURL", kGADAdSenseAppWebContentURL,
+                     @"googleAdSenseChannelIDs", kGADAdSenseChannelIDs,
+                     @"googleAdSenseAdType", kGADAdSenseAdType,
+                     @"googleAdSenseHostID", kGADAdSenseHostID,
+                     @"adWhirlAdBackgroundColor", kGADAdSenseAdBackgroundColor,
+                     @"googleAdSenseAdBorderColor", kGADAdSenseAdBorderColor,
+                     @"googleAdSenseAdLinkColor", kGADAdSenseAdLinkColor,
+                     @"adWhirlTextColor", kGADAdSenseAdTextColor,
+                     @"googleAdSenseAdURLColor", kGADAdSenseAdURLColor,
+                     @"googleAdSenseExpandDirection", kGADExpandDirection,
+                     @"googleAdSenseAlternateAdColor", kGADAdSenseAlternateAdColor,
+                     @"googleAdSenseAlternateAdURL", kGADAdSenseAlternateAdURL,
+                     nil];
+}
 
-- (void)getAd {
-  //*** TODO ***
-  NSLog(@"Google AdSense getAd");
-  [super getAd];
+- (id)initWithAdWhirlDelegate:(id<AdWhirlDelegate>)delegate
+                         view:(AdWhirlView *)view
+                       config:(AdWhirlConfig *)config
+                networkConfig:(AdWhirlAdNetworkConfig *)netConf {
+  self = [super initWithAdWhirlDelegate:delegate
+                                   view:view
+                                 config:config
+                          networkConfig:netConf];
+  if (self != nil) {
+    // Check that the required methods are implemented.
+    if (![delegate respondsToSelector:@selector(googleAdSenseCompanyName)]) {
+      [NSException raise:NSInvalidArgumentException format:
+       @"You must implement googleAdSenseCompanyName in your AdwhirlDelegate in order to use Google AdSense"];
+    }
+    if (![delegate respondsToSelector:@selector(googleAdSenseAppName)]) {
+      [NSException raise:NSInvalidArgumentException format:
+       @"You must implement googleAdSenseAppName in your AdwhirlDelegate in order to use Google AdSense"];
+    }
+  }
+  return self;
+}
+
+- (void)getAd {	
+  adViewController = [[GADAdViewController alloc] initWithDelegate:self];
+
+	NSMutableDictionary *attributes = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                     [self publisherId], kGADAdSenseClientID,
+                                     [self companyName], kGADAdSenseCompanyName,
+                                     [self appName], kGADAdSenseAppName,
+                                     [self testMode], kGADAdSenseIsTestAdRequest,
+                                     nil];
+
+  // optional params
+  for (NSString *paramName in GASParamNameToSel) {
+    SEL sel = NSSelectorFromString((NSString *)[GASParamNameToSel objectForKey:paramName]);
+    if ([adWhirlDelegate respondsToSelector:sel]) {
+      NSObject *val = [adWhirlDelegate performSelector:sel];
+      if (val != nil) {
+        [attributes setObject:val forKey:paramName];
+      }
+    }
+  }
+
+  AWLogDebug(@"Google AdSense attributes: %@", attributes);
+	// load the ad
+	adViewController.adSize = kGADAdSize320x50;
+	[adViewController loadGoogleAd:attributes];
+  adViewController.view.frame = CGRectMake(0, 0, 320, 50);
+	self.adNetworkView = adViewController.view;
 }
 
 - (void)dealloc {
-  [super dealloc];
+  [adViewController release];
+	[super dealloc];
 }
+
+#pragma mark parameter gathering methods
+
+- (NSString *)publisherId {
+	if ([adWhirlDelegate respondsToSelector:@selector(googleAdSenseClientID)]) {
+		return [adWhirlDelegate googleAdSenseClientID];
+	}
+	
+	return networkConfig.pubId;
+}
+
+- (NSString *)companyName {
+	return [adWhirlDelegate googleAdSenseCompanyName];
+}
+
+- (NSString *)appName {
+	return [adWhirlDelegate googleAdSenseAppName];
+}
+
+- (NSNumber *)testMode {
+  if ([adWhirlDelegate respondsToSelector:@selector(adWhirlTestMode)]
+      && [adWhirlDelegate adWhirlTestMode]) {
+    return [NSNumber numberWithInt:1];
+  }
+  return [NSNumber numberWithInt:0];
+}
+
+#pragma mark GADAdViewControllerDelegate required methods
+
+- (UIViewController *)viewControllerForModalPresentation:(GADAdViewController *)adController {
+	return [adWhirlDelegate viewControllerForPresentingModalView];
+}
+
+#pragma mark GADAdViewControllerDelegate notification methods
+
+- (void)loadSucceeded:(GADAdViewController *)adController withResults:(NSDictionary *)results {
+  [self helperFitAdNetworkView];
+	[adWhirlView adapter:self didReceiveAdView:[adController view]];
+}
+
+- (void)loadFailed:(GADAdViewController *)adController withError:(NSError *) error {	
+	[adWhirlView adapter:self didFailAd:error];
+}
+
+- (GADAdClickAction)adControllerActionModelForAdClick:(GADAdViewController *)adController {
+  [self helperNotifyDelegateOfFullScreenModal];
+	return GAD_ACTION_DISPLAY_INTERNAL_WEBSITE_VIEW; // full screen web view
+}
+
+- (void)adControllerDidCloseWebsiteView:(GADAdViewController *)adController {
+  [self helperNotifyDelegateOfFullScreenModalDismissal];
+}
+
+//- (void)adControllerDidExpandAd:(GADAdViewController *)controller {
+//}
+//
+//- (void)adControllerDidCollapseAd:(GADAdViewController *)controller {
+//}
 
 @end
