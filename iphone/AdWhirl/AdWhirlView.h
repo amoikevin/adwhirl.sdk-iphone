8f78a49217fbbbffbe0fc9cbbef39f703de14537
Nigel Choi <nigelchoi@google.com>
2010-09-20 14:13:30 -0700

    Bump version to 260 and updated change log for 2.6.0.
    R=jpincar
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=225789

--- iphone/AdWhirl/AdWhirlView.h
+++ iphone/AdWhirl/AdWhirlView.h
@@ -23,7 +23,7 @@
 #import "AWNetworkReachabilityWrapper.h"
 #import "AdWhirlConfig.h"
 
-#define kAdWhirlAppVer 255
+#define kAdWhirlAppVer 260
 
 #define kAdWhirlViewWidth 320
 #define kAdWhirlViewHeight 50
