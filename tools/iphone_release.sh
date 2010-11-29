5f328c738e1be0a86c2cc1cf5421c4f8aeaaefb0
nigelchoi <nigelchoi@google.com>
2010-11-29 22:19:51 +0000

    No need to copy the instructions PDF any more when packaging iOS releases.

--- tools/iphone_release.sh
+++ tools/iphone_release.sh
@@ -2,18 +2,12 @@
 
 set -o errexit
 
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <version> <path to instructions file>"
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <version>"
   exit 1
 fi
 
 ver=$1
-instr_file=$2
-
-if [ ! -f $instr_file ]; then
-  echo "Instruction file $instr_file not a file"
-  exit 1
-fi
 
 dir=AdWhirlSDK_iPhone_$ver
 rm -rf $dir
@@ -23,5 +17,4 @@ cp -r iphone/AdWhirlSDK2_Sample $dir/
 cp -r iphone/TouchJSON $dir/
 cp iphone/Changelog.txt $dir/
 cp iphone/README $dir/
-cp $instr_file $dir/
 zip -r $dir.zip $dir
