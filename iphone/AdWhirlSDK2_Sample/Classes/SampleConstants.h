298562e794ce0561060c38e5561b168e6691716a
wesgoodman <wesgoodman@google.com>
2011-11-15 11:52:08 -0800

    Added support for Nexage network Updated InMobi adapter to be compliant with v300 Updated Millennial adapter to be compliant with v4.2.6 Updated JumpTap adapter to be compliant with v2.0.14.1 Fixed Reachability NPE (Issue #118) Fixed potential NSError null dereference (Issue #179)
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=

--- iphone/AdWhirlSDK2_Sample/Classes/SampleConstants.h
+++ iphone/AdWhirlSDK2_Sample/Classes/SampleConstants.h
@@ -1,7 +1,7 @@
 /*
 
  SampleConstants.h
- 
+
  Copyright 2009 AdMob, Inc.
 
  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,11 +15,13 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
- 
+
 */
 
-#define kSampleAppKey @"960486d703f8102d96dc5b26aef5c1e9"
-//#define kSampleAppKey @"2e8d7eed0b1b102d96dc5b26aef5c1e9"
+#if !defined(kSampleAppKey)
+  #error "You must define kSampleAppKey as your AdWhirl SDK Key"
+#endif
+
 #define kSampleConfigURL @"http://mob.adwhirl.com/getInfo.php"
 #define kSampleImpMetricURL @"http://met.adwhirl.com/exmet.php"
 #define kSampleClickMetricURL @"http://met.adwhirl.com/exclick.php"
